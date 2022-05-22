424c1f338d8000ba2fb1e615f7b3bedaa79fee09
Uygar Süslü <uygarsuslu1@gmail.com>
2022-05-22 13:31:03 +0300

    watch list added

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { MovieCard } from './MovieCard'
 
 export const Watchlist = () => {
+
+  const { watchlist } = useContext(GlobalContext)
+
   return (
-    <div>
-      <h1>Watchlist Page</h1>
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">My Watchlist</h1>
+
+          <span className="count-pill">
+            {watchlist.length} {watchlist.length === 1 ? "Movie" : "Movies"}
+          </span>
+        </div>
+
+        {watchlist.length > 0 ? (
+          <div style={{ height: "65vh", overflow: "hidden", overflowY: (watchlist.length < 6) ? "hidden" : "scroll" }}>
+            <div className='movie-grid'>
+              {watchlist.map(movie => (
+                <MovieCard movie={movie} type="watchlist" />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <h2 className='no-movies'>No movies in your list, add some!</h2>
+        )}
+      </div>
     </div>
   )
 }
