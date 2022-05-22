13f416e5706160bac8c9d5304678016980891da9
Uygar Süslü <uygarsuslu1@gmail.com>
2022-05-22 13:31:09 +0300

    watched added

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { MovieCard } from "./MovieCard"
 
 export const Watched = () => {
+
+  const { watched } = useContext(GlobalContext)
+
   return (
-    <div>
-      <h1>Watched Page</h1>
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">Watched Movies</h1>
+
+          <span className="count-pill">
+            {watched.length} {watched.length === 1 ? "Movie" : "Movies"}
+          </span>
+        </div>
+
+        {watched.length > 0 ? (
+          <div style={{ height: "65vh", overflow: "hidden", overflowY: (watched.length < 6) ? "hidden" : "scroll" }}>
+            <div className='movie-grid'>
+              {watched.map(movie => (
+                <MovieCard movie={movie} type="watched" />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <h2 className='no-movies'>No movies in your list, add some!</h2>
+        )}
+      </div>
     </div>
   )
 }
